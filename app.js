c643c6e74b6953ea3e9f079a65206c4ee8a161f9
chen zeng <49711899@qq.com>
2019-12-05 10:39:03 +0800

    add migration setting

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const apiRouter = require('./routes/api')
 const dbconfig = require('./db')
 // db setup
 dbconfig()
+// console.log(process.env)
 // setup routes
 app.use('/', indexRouter)
 app.use('/api', apiRouter)
